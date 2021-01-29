f9a1bdbee61776b1d66c402c14ac52a79c0a4bb6
Jhanvi Dodiya <40354418+jhanvi0818@users.noreply.github.com>
2021-01-29 16:15:08 +0530

    Add files via upload

---
+++ MavenJenkins/src/main/java/com/jmaven/HelloJenkins.java
@@ -0,0 +1,10 @@
+package com.jmaven;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First maven project in jenkins..");
+	}
+
+}
